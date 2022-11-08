ae318e1a66edcd4c223c2aaeb1b280023d0eed0c
yusiny <yusinkim.or@gmail.com>
2022-11-08 18:10:37 +0900

    test: JDBC 사용 테스트

--- app/src/main/java/com/database_termproject/twitter/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/database_termproject/twitter/ui/main/home/HomeFragment.kt
@@ -3,8 +3,21 @@ package com.database_termproject.twitter.ui.main.home
 import com.database_termproject.twitter.databinding.FragmentHomeBinding
 import com.database_termproject.twitter.ui.BaseFragment
 
-class HomeFragment: BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-    override fun initAfterBinding() {
 
+class HomeFragment: BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    private val DATABASE = "mydb"
+    private val USER = "root"
+    private val PASSWORD = "021019@wa"
+
+    override fun initAfterBinding() {
+        binding.homeBtn.setOnClickListener {
+            val userId = binding.idEt.text
+            val password = binding.pwEt.text
+
+
+            val query =
+                "select user_id from user where user_id = \"$userId\" and password=\"$password\""
+
+        }
     }
 }
--- app/src/main/java/com/database_termproject/twitter/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/database_termproject/twitter/ui/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.database_termproject.twitter.ui.splash
 import android.os.Handler
 import com.database_termproject.twitter.databinding.ActivitySplashBinding
 import com.database_termproject.twitter.ui.BaseActivity
+import com.database_termproject.twitter.ui.main.ExampleActivity
 import com.database_termproject.twitter.ui.main.MainActivity
 
 
@@ -10,7 +11,7 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding
     override fun initAfterBinding() {
         // 1초 후, Main Activity로 이동
         Handler().postDelayed(Runnable {
-            startNextActivity(MainActivity::class.java)
+            startNextActivity(ExampleActivity::class.java)
             finish()
         }, 1000)
     }
