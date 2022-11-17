49cebb8fb15683534d477fc0c81aee3f07f7ef92
yusiny <yusinkim.or@gmail.com>
2022-11-18 05:54:22 +0900

    SharedPreferenceManager 추가

--- app/src/main/java/com/database_termproject/twitter/utils/GlobalApplication.kt
+++ app/src/main/java/com/database_termproject/twitter/utils/GlobalApplication.kt
@@ -1,11 +1,25 @@
 package com.database_termproject.twitter.utils
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import com.database_termproject.twitter.BuildConfig
 
-class GlobalApplication {
+class GlobalApplication: Application() {
     companion object{
+        const val TAG = "Twitter";
         const val USER = BuildConfig.USER;          // user 이름
         const val PASSWORD = BuildConfig.PASSWORD; // password
         const val URL = BuildConfig.URL; // url
+
+
+        lateinit var mSharedPreferences: SharedPreferences         //APP 기본 SharedPreference
     }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //앱 sharedPreference
+        mSharedPreferences = applicationContext.getSharedPreferences(TAG, Context.MODE_PRIVATE)
+}
 }
