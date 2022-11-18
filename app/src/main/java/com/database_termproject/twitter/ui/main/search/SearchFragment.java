7fad03408030e3f9ac3033eaeeb9c9145946aa0b
yusiny <yusinkim.or@gmail.com>
2022-11-19 07:05:31 +0900

    유저 검색 JDBC

--- app/src/main/java/com/database_termproject/twitter/ui/main/search/SearchFragment.java
+++ app/src/main/java/com/database_termproject/twitter/ui/main/search/SearchFragment.java
@@ -1,17 +1,13 @@
 package com.database_termproject.twitter.ui.main.search;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.database_termproject.twitter.R;
+import androidx.navigation.NavDirections;
 import com.database_termproject.twitter.databinding.FragmentSearchBinding;
 import com.database_termproject.twitter.ui.BaseFragment;
+
+import java.util.Locale;
 
 public class SearchFragment extends BaseFragment<FragmentSearchBinding> {
     @Override
@@ -25,16 +21,13 @@ public class SearchFragment extends BaseFragment<FragmentSearchBinding> {
     }
 
     private void setMyClickListener(){
-        Log.d("search0", "현재 위치 - " + findNavController().getCurrentDestination());
-
-        // 검색 입력 텍스트뷰 클릭 시,
-        binding.searchTv.setOnClickListener(new View.OnClickListener() {
+        // 검색 클릭 시,
+        binding.searchIv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Log.d("search1", "현재 위치 - " + findNavController().getCurrentDestination());
-                findNavController().navigate(R.id.action_searchFragment_to_searchBeforeFragment);
-                Log.d("search2", "현재 위치 - " + findNavController().getCurrentDestination());
+                String searchWord = binding.searchEt.getText().toString();
+                NavDirections action = SearchFragmentDirections.actionSearchFragmentToSearchAfterFragment(searchWord);
+                findNavController().navigate(action);
             }
         });
     }
