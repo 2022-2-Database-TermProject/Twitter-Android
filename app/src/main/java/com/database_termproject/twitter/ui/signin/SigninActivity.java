1b559cc23ac00d137e018005de41e589cba5504e
juhye87 <78808933+juhye87@users.noreply.github.com>
2022-11-20 00:41:40 +0900

    비밀번호 찾기

--- app/src/main/java/com/database_termproject/twitter/ui/signin/SigninActivity.java
+++ app/src/main/java/com/database_termproject/twitter/ui/signin/SigninActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.database_termproject.twitter.R;
@@ -46,8 +47,19 @@ public class SigninActivity extends AppCompatActivity {
             }
         });
 
+        TextView findPWButton = findViewById(R.id.find_pw_btn);
+        findPWButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent myIntent = new Intent(getApplicationContext(), FindPassWordActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
         editTextID = findViewById(R.id.textInputEditTextID);
         editTextPW = findViewById(R.id.textInputEditTextPassword);
+
 
         loginButton = findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
