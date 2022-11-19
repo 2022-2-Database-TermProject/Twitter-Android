7862da1f46133555efabd8808491d121563642ff
juhye87 <78808933+juhye87@users.noreply.github.com>
2022-11-19 11:10:03 +0900

    로그인 연동한거

--- app/src/main/java/com/database_termproject/twitter/ui/signin/SigninActivity.java
+++ app/src/main/java/com/database_termproject/twitter/ui/signin/SigninActivity.java
@@ -3,6 +3,7 @@ package com.database_termproject.twitter.ui.signin;
 import static com.database_termproject.twitter.utils.GlobalApplication.PASSWORD;
 import static com.database_termproject.twitter.utils.GlobalApplication.URL;
 import static com.database_termproject.twitter.utils.GlobalApplication.USER;
+import static com.database_termproject.twitter.utils.SharedPreferenceManagerKt.saveUserId;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.database_termproject.twitter.R;
 import com.database_termproject.twitter.ui.main.MainActivity;
@@ -44,18 +46,17 @@ public class SigninActivity extends AppCompatActivity {
             }
         });
 
+        editTextID = findViewById(R.id.textInputEditTextID);
+        editTextPW = findViewById(R.id.textInputEditTextPassword);
+
         loginButton = findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 new SigninAsyncTask().execute();
-
-                Log.d("ID", editTextID.getText().toString());
-                Log.d("PW", editTextPW.getText().toString());
             }
         });
-
     }
 
     public class SigninAsyncTask extends AsyncTask<Void, Void, String> {
@@ -67,25 +68,36 @@ public class SigninActivity extends AppCompatActivity {
             String password = editTextPW.getText().toString();
 
             try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-                String query = "select id from user where id = \"" + userId + "\"";
+                String query = "select id from user where id = \"" + userId + "\" and pwd=\"" + password + "\"";
 
                 Statement stmt = connection.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
 
                 if (rs.next()) {
-                    result = "Already exist!";
+                    result = "Login success!" + userId;
                 }else{
+                    String s2 = "Select id from user where id = \""+ userId+"\"";
+
+                    Statement stmt1 = connection.createStatement();
+                    ResultSet rs1 = stmt1.executeQuery(s2);
+
+                    if (rs1.next()) {
+                        result = "Wrong Password, Do it again";
+                    }
+                    else {
+                        result = "Wrong ID";
+
+                    }
 
 
                     PreparedStatement pstm = connection.prepareStatement(query);
                     pstm.executeUpdate();
 
-                    result ="Sign up complete";
+                    result ="Sign in complete";
                 }
             } catch (Exception e) {
                 Log.e("InfoAsyncTask", "Error reading school information", e);
             }
-
 
             return result;
         }
@@ -95,8 +107,18 @@ public class SigninActivity extends AppCompatActivity {
 
             this.cancel(true);
 
-            Intent myIntent = new Intent(getApplicationContext(),InterestActivity.class);
-            startActivity(myIntent);
+            String[] array = result.split("!");
+
+
+            if (array[0].equals("Login success")) {
+                Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(myIntent);
+
+                saveUserId(array[1]);
+            }
+            else {
+                Log.d("success?", "wrong");
+            }
 
         }
     }
